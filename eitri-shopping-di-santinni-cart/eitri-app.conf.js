42e16d08733d2adbe62d430408752bdd80d36ecb
Matheus <matheus150101miranda@gmail.com>
2026-04-28 17:25:05 -0300

    fix: up version

--- eitri-shopping-di-santinni-cart/eitri-app.conf.js
+++ eitri-shopping-di-santinni-cart/eitri-app.conf.js
@@ -5,7 +5,8 @@ module.exports = {
 	'eitri-luminus': '2.22.6',
 	'eitri-bifrost': '5.1.0',
 	'eitri-commons': '2.2.1',
-	'version': '0.1.0',
+	'version': '0.1.1',
+	'versionMessage': 'Atualizei/refatorei componentes',
 	'public-key': 'df2b9d84-df8a-4fc9-835f-a8c90e026c2d',
 	'applicationId': 'f0301a60-5a72-487b-8855-a4fbd21d1f39',
 	'id': 'dc1da343-fcfa-4618-9bc0-173d1d5889c8',
@@ -14,7 +15,7 @@ module.exports = {
 	'eitri-app-dependencies': {
 		'eitri-shopping-di-santinni-shared': {
 			isEitriAppShared: true,
-			version: '0.1.0'
+			version: '0.1.1'
 		},
 		'eitri-shopping-vtex-shared': {
 			isEitriAppShared: true,
