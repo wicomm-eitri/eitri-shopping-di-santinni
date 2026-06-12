cf0540a3f44f59ac212d3c8d45b3fcd6bb7eb53d
EduardoBalbinotDev <914thepro@gmail.com>
2026-06-12 16:42:33 -0300

    chore: eitri-app.conf.js updated

--- eitri-shopping-di-santinni-shared/eitri-app.conf.js
+++ eitri-shopping-di-santinni-shared/eitri-app.conf.js
@@ -5,7 +5,7 @@ module.exports = {
 	'eitri-luminus': '2.22.6',
 	'eitri-bifrost': '5.1.0',
 	'eitri-commons': '2.2.1',
-	'version': '0.1.7',
+	'version': '0.1.8',
 	'versionMessage': 'correção de QA no custom carrousel',
 	'public-key': 'dc1f3bd6-04a5-4967-8322-82f611bf156d',
 	'applicationId': 'f0301a60-5a72-487b-8855-a4fbd21d1f39',
