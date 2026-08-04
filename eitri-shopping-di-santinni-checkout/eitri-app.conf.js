74717406ac2c06b5535129c8efcdd27a73170012
João Marcelo Carvalho <joaomarceloc03@icloud.com>
2026-08-04 12:55:57 -0300

    fix: recreate shared app version (#60)

--- eitri-shopping-di-santinni-checkout/eitri-app.conf.js
+++ eitri-shopping-di-santinni-checkout/eitri-app.conf.js
@@ -5,8 +5,8 @@ module.exports = {
 	'eitri-luminus': '2.22.6',
 	'eitri-bifrost': '5.1.0',
 	'eitri-commons': '2.2.1',
-	'version': '0.1.15',
-	'versionMessage': 'Ajustes QA 04/08',
+	'version': '0.1.16',
+	'versionMessage': 'Correção da versão shared',
 	'public-key': 'd0437867-ce83-45da-ada6-8253035b5d20',
 	'applicationId': 'f0301a60-5a72-487b-8855-a4fbd21d1f39',
 	'id': '8843ccd9-f15d-4904-822d-fba5eebd93d3',
@@ -15,7 +15,7 @@ module.exports = {
 	'eitri-app-dependencies': {
 		'eitri-shopping-di-santinni-shared': {
 			isEitriAppShared: true,
-			version: '0.1.15'
+			version: '0.1.16'
 		},
 		'eitri-shopping-vtex-shared': {
 			isEitriAppShared: true,
