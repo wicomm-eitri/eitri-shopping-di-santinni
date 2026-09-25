b469d1b95471ecc636b94a76a6a53a90c8e9c16a
uendelpapa-wicomm <uendel.papa@wicomm.com.br>
2026-09-25 11:13:07 -0300

    feat: tela permissão

--- eitri-shopping-di-santinni-cartao/src/components/CardHeader/CardHeader.jsx
+++ eitri-shopping-di-santinni-cartao/src/components/CardHeader/CardHeader.jsx
@@ -2,10 +2,43 @@ import { View, Image } from 'eitri-luminus'
 import { HeaderContentWrapper, HeaderReturn, HeaderLogo } from 'eitri-shopping-di-santinni-shared'
 import logo from '../../assets/images/logoHeader.png'
 import MenuIcon from '../../assets/icons/menu.svg'
+import CloseIcon from '../../assets/icons/close.svg'
 import { openMenu } from '../../services/NavigationService'
 
 export default function CardHeader(props) {
-	const { onBack, showMenu = true } = props
+	const { onBack, onClose, showMenu = true } = props
+
+	const renderRightAction = () => {
+		if (typeof onClose === 'function') {
+			return (
+				<View
+					className='flex items-center'
+					onClick={onClose}>
+					<Image
+						src={CloseIcon}
+						alt='Fechar'
+						className='w-6 h-6'
+					/>
+				</View>
+			)
+		}
+
+		if (showMenu) {
+			return (
+				<View
+					className='flex items-center'
+					onClick={openMenu}>
+					<Image
+						src={MenuIcon}
+						alt='Menu'
+						className='w-6 h-6'
+					/>
+				</View>
+			)
+		}
+
+		return <View className='w-6 h-6' />
+	}
 
 	return (
 		<HeaderContentWrapper
@@ -16,19 +49,7 @@ export default function CardHeader(props) {
 				onClick={onBack}
 			/>
 			<HeaderLogo src={logo} />
-			{showMenu ? (
-				<View
-					className='flex items-center'
-					onClick={openMenu}>
-					<Image
-						src={MenuIcon}
-						alt='Menu'
-						className='w-6 h-6'
-					/>
-				</View>
-			) : (
-				<View className='w-6 h-6' />
-			)}
+			{renderRightAction()}
 		</HeaderContentWrapper>
 	)
 }
--- eitri-shopping-di-santinni-cartao/src/services/NavigationService.js
+++ eitri-shopping-di-santinni-cartao/src/services/NavigationService.js
@@ -25,7 +25,8 @@ export const PAGES = {
 	MY_CARD: '/MyCard',
 	PERSONAL_DATA: '/PersonalData',
 	REGISTER_BIOMETRICS: '/RegisterBiometrics',
-	REGISTER_IDENTITY: '/RegisterIdentity'
+	REGISTER_IDENTITY: '/RegisterIdentity',
+	REGISTER_PERMISSION: '/RegisterPermission'
 }
 
 export const navigate = (page, state = {}, replace = false) => {
